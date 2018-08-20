370e28238cf592ff78ea01126ade7ce4f3aa54a2
prakashdanish <grafitykoncept@gmail.com>
2018-08-20 11:45:27 +0530

    Docstrings, unavailable conditions

--- pip-name.py
+++ pip-name.py
@@ -7,28 +7,31 @@ BASE_URL = 'https://pypi.org/pypi'
 
 
 def get_response(name):
+    """Request response from PyPi API"""
     target_url = '{0}/{1}/json'.format(BASE_URL, name)
     r = requests.get(target_url)
-    
-    # todo handle exceptions
-    return r.json()
+
+    if r.status_code == 404:
+        response = None
+    else:
+        response = r.json()
+
+    return response
 
 
 def is_name_taken(name):
+    """Check module filename for conflict"""
     response = get_response(name)
 
-    if response == '404':
-        return False
+    if response:
+        file_name = response.get('urls')[0].get('filename').split('-')[0]
+        return True if name.lower() == file_name.lower() else False
 
-    file_name = response.get('urls')[0].get('filename').split('-')[0]
-
-    # check filename of the found package
-    if name == file_name:
-        return True
     return False
 
 
 def main():
+    """Handle arguments and flow"""
     parser = argparse.ArgumentParser()
     parser.add_argument('name', help='name of the package you want to check', type=str)
     args = parser.parse_args()
@@ -36,7 +39,8 @@ def main():
     if is_name_taken(args.name):
         print('`{0}` is unavailable.'.format(args.name))
     else:
-        print('{0} is available'.format(args.name))
+        print('`{0}` is available'.format(args.name))
+
 
 if __name__ == '__main__':
     main()
