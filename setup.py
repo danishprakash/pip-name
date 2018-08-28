3017b230a535effe388c5ab0f9e927f2ac537e8a
prakashdanish <grafitykoncept@gmail.com>
2018-08-28 13:36:05 +0530

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='pip-name',
+    version='1.0.0',
+    description='Check whether package name is available on PyPi',
+    long_description=long_description,
+    author='Danish Prakash',
+    author_email='danishprakash@outlook.com',
+    url='https://github.com/prakashdanish/pip-name',
+    packages=['pip_name'],
+    install_requires=[
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pip-name = pip_name.pip_name:main'
+        ],
+    },
+)
